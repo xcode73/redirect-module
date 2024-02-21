f62d2e3237d68ede5e6ac76b4577271381945182
xcode73 <xcode.uln@gmail.com>
2024-02-21 08:49:57 +0400

    change to xcode73 test-dev

--- Package.swift
+++ Package.swift
@@ -11,12 +11,12 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/xcode73/feather-core", .branch("test-dev")),
-        .package(url: "https://github.com/xcode73/feather-objects", .branch("test-dev")),
+        .package(url: "https://github.com/xcode73/redirect-objects", .branch("test-dev")),
     ],
     targets: [
         .target(name: "RedirectModule", dependencies: [
             .product(name: "Feather", package: "feather-core"),
-            .product(name: "FeatherObjects", package: "feather-objects"),
+            .product(name: "RedirectObjects", package: "redirect-objects"),
         ],
         resources: [
 //            .copy("Bundle"),
