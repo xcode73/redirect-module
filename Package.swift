00259975f82db7c3b7be5085d328741466e7f71c
Nikolai Eremenko <nikolai.dev@icloud.com>
2024-06-07 10:37:57 +0400

    build: update dependencies

--- Package.swift
+++ Package.swift
@@ -4,22 +4,22 @@ import PackageDescription
 let package = Package(
     name: "redirect-module",
     platforms: [
-       .macOS(.v12),
+       .macOS(.v12)
     ],
     products: [
         .library(name: "RedirectModule", targets: ["RedirectModule"]),
     ],
     dependencies: [
-        .package(path: "../feather-core"),
-//        .package(url: "git@github.com:xcode73/feather-core.git", branch: "main")
+        .package(path: "../feather-core")
+//        .package(url: "https://github.com/xcode73/feather-core.git", branch: "main")
     ],
     targets: [
         .target(name: "RedirectApi", dependencies: [
-            .product(name: "FeatherCoreApi", package: "feather-core"),
+            .product(name: "FeatherCoreApi", package: "feather-core")
         ]),
         .target(name: "RedirectModule", dependencies: [
             .product(name: "FeatherCore", package: "feather-core"),
-            .target(name: "RedirectApi"),
+            .target(name: "RedirectApi")
         ],
         resources: [
 //            .copy("Bundle"),
